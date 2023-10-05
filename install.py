2592e01dcef9997378a16d0957c685d66cf6b66e
aria1th <35677394+aria1th@users.noreply.github.com>
2023-10-06 00:06:42 +0900

    fix install.py

--- install.py
+++ install.py
@@ -3,5 +3,5 @@ import torch.cuda as cuda
 # launch is imported in context of webui
 if not launch.is_installed("onnxruntime") and not launch.is_installed("onnxruntime-gpu"):
     print("Installing onnxruntime")
-    launch.install("onnxruntime-gpu" if cuda.is_available() else "onnxruntime")
+    launch.run_pip("install onnxruntime-gpu" if cuda.is_available() else "install onnxruntime")
 
