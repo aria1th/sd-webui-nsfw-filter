2dc2e56aadd3d169f41e6505358cc9cb9e650b50
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-10-22 20:47:29 +0900

    reduce load time, Import torch only when necessary

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 import launch
-import torch.cuda as cuda
 # launch is imported in context of webui
 if not launch.is_installed("onnxruntime") and not launch.is_installed("onnxruntime-gpu"):
+    import torch.cuda as cuda
     print("Installing onnxruntime")
     launch.run_pip("install onnxruntime-gpu" if cuda.is_available() else "install onnxruntime")
-
